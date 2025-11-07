740979ce0c365c15949c737aa096523e6c027aeb
dhilip <dkdhilip777@gmail.com>
2025-11-07 22:57:28 +0530

    added frontend url

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,7 @@ app = FastAPI(
 
 origins = [
     "http://localhost:5173",
-    "https://llm-retriever.onrender.com"
+    "https://quickread-renk.onrender.com"
 ]
 # CORS middleware
 app.add_middleware(
